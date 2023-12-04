5443ac968cf1f28df09760ad9559bcef2aa37414
Magomed-Workout <128885397+Magomed-Workout@users.noreply.github.com>
2023-12-04 16:46:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const slider = document.querySelector('.swiper-container');
+
+let mySwiper = new Swiper(slider, {
+    sliderPerView: 1,
+    spaceBetween: 100,
+    loop: false,
+    lazy: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000000,
+    },
+}); 
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('burger').addEventListener('click', function() {
+        document.querySelector('.header').classList.toggle('open')
+    })
+});
+
+document.getElementById('nav').addEventListener('click', event => {
+    event._isClickWithInMenu = true;
+})
+
+document.getElementById('burger').addEventListener('click', event => {
+    event._isClickWithInMenu = true;
+})
+
+document.body.addEventListener('click', event => {
+    if (event._isClickWithInMenu) return;
+    document.querySelector('.header').classList.remove('open')
+});
